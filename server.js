52eaf49f65855499b93adb23469268774967fddf
YuvrajDesh <yuvrajdeshmukh8998@gmail.com>
2022-11-03 23:54:50 +0530

    added auth and cart page

--- server.js
+++ server.js
@@ -5,15 +5,36 @@ const res = require('express/lib/response')
 const p  = require('path')
 const app = express()
 const PORT = process.env.PORT || 3000
+
 // assets
 app.use(express.static('public'))
-app.get('/',(req,res) =>{
-    res.render(`home`)
-})
 //set  template engine
 app.use(expressLayout)
 app.set('views',p.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
+
+// app.get('/',(req,res) =>{
+//     res.render(`home`)
+// })
+app.get('/cart',(req,res) =>{
+    res.render(`./customer/cart`)
+})
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
+
+app.get('/login', (req, res) => {
+    res.render('auth/login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('auth/register')
+})
 
 app.listen(PORT,()=>{
     console.log(`Listening on port  ${PORT}`)
